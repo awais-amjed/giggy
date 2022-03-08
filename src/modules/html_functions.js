83f8b4379a7e0117e235f2725c46dac9f7484a1d
Awais Amjed <awaisamjed171@gmail.com>
2022-03-08 18:37:23 +0500

    Add Colors to Joke Cards & Fix Card text

--- src/modules/html_functions.js
+++ src/modules/html_functions.js
@@ -1,5 +1,18 @@
 import JokeAPI from './joke_api.js';
 
+const colorsArray = [
+  '#005F99',
+  '#D22779',
+  '#7900FF',
+  '#1E3163',
+  '#1DB9C3',
+  '#72147E',
+  '#252525',
+  '#153E90',
+  '#DB66E4',
+  '#3E978B',
+];
+
 const populateJokes = async ({ category = 'Dark' }) => {
   let jokes = [];
 
@@ -9,17 +22,19 @@ const populateJokes = async ({ category = 'Dark' }) => {
     jokes = await JokeAPI.getJokes(JokeAPI.jokeCategories.programming);
   }
 
+  let i = 0;
   jokes.forEach(({ setup, delivery, joke }) => {
     const jokeContainer = document.createElement('div');
     jokeContainer.classList.add('joke-container', 'col-lg-4', 'col-md-6', 'col-xxl-3');
 
     const jokeNode = document.createElement('div');
     jokeNode.classList.add('joke-card');
+    jokeNode.style['background-color'] = colorsArray[i];
 
     jokeContainer.appendChild(jokeNode);
     jokeNode.innerHTML = `
       <div class="joke-text">
-        ${joke ? `<p>${joke}</p>` : `<p>${setup}</p><p>${delivery}</p>`}
+        ${joke ? `<p>${joke.replaceAll('\n', '<br>')}</p>` : `<p>${setup}</p><p>${delivery}</p>`}
         <hr>
       </div>
     `;
@@ -29,6 +44,7 @@ const populateJokes = async ({ category = 'Dark' }) => {
 
     jokeNode.appendChild(commentsButton);
     document.getElementById('jokes-list').appendChild(jokeContainer);
+    i += 1;
   });
 };
 

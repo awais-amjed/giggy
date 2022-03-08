643e05ab97f1a45f7454ed9ba0d3b3193068df0d
Awais Amjed <awaisamjed171@gmail.com>
2022-03-08 18:07:45 +0500

    Add Bootstrap to make list responsive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import populateJokes from './modules/html_functions.js';
 
--- src/modules/html_functions.js
+++ src/modules/html_functions.js
@@ -10,8 +10,13 @@ const populateJokes = async ({ category = 'Dark' }) => {
   }
 
   jokes.forEach(({ setup, delivery, joke }) => {
+    const jokeContainer = document.createElement('div');
+    jokeContainer.classList.add('joke-container', 'col-lg-4', 'col-md-6', 'col-xxl-3');
+
     const jokeNode = document.createElement('div');
     jokeNode.classList.add('joke-card');
+
+    jokeContainer.appendChild(jokeNode);
     jokeNode.innerHTML = `
       <div class="joke-text">
         ${joke ? `<p>${joke}</p>` : `<p>${setup}</p><p>${delivery}</p>`}
@@ -23,7 +28,7 @@ const populateJokes = async ({ category = 'Dark' }) => {
     commentsButton.innerText = 'Comments';
 
     jokeNode.appendChild(commentsButton);
-    document.getElementById('jokes-list').appendChild(jokeNode);
+    document.getElementById('jokes-list').appendChild(jokeContainer);
   });
 };
 
